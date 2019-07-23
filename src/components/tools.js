f181cfdc1a7b483bb1e1eb92978c2482afbadcfd
Jonathan Edward Lee <jonathan.lee02@sap.com>
2019-07-23 09:10:06 +0200

    update tools component

--- src/components/tools.js
+++ src/components/tools.js
@@ -11,12 +11,68 @@ class Tools extends React.Component {
         defaultPosition={{ x: 0, y: 0 }}
         position={null}
         scale={1}
-        bounds={{ left: -2000, right: 0 }}
+        bounds={{ left: -4000, right: 0 }}
         onStart={this.handleStart}
         onDrag={this.handleDrag}
         onStop={this.handleStop}
       >
         <div className="Tools handle">
+          <Tool
+            class="Tool STN"
+            link="/stn"
+            title="SAP Transformation Navigator"
+            logo={require("../images/stn.svg")}
+            image={require("../images/stn.jpg")}
+          />
+          <Tool
+            class="Tool Sirius"
+            link="/sirius"
+            title="Sirius"
+            logo={require("../images/sirius.svg")}
+            image={require("../images/sirius.jpg")}
+          />
+          <Tool
+            class="Tool PPMS"
+            link="/ppms"
+            title="PPMS Light"
+            logo={require("../images/ppms.svg")}
+            image={require("../images/ppms.jpg")}
+          />
+          <Tool
+            class="Tool Polaris"
+            link="/polaris"
+            title="Polaris"
+            logo={require("../images/polaris.svg")}
+            image={require("../images/polaris.jpg")}
+          />
+          <Tool
+            class="Tool Lucid"
+            link="/lucid"
+            title="Lucid"
+            logo={require("../images/lucid.svg")}
+            image={require("../images/lucid.jpg")}
+          />
+          <Tool
+            class="Tool Solaris"
+            link="/solaris"
+            title="Solaris"
+            logo={require("../images/solaris.svg")}
+            image={require("../images/solaris.jpg")}
+          />
+          <Tool
+            class="Tool Toby"
+            link="/tobi"
+            title="Tobi"
+            logo={require("../images/toby.svg")}
+            image={require("../images/toby.jpg")}
+          />
+          <Tool
+            class="Tool IFR"
+            link="/ifr"
+            title="Innovation & Feature Repository"
+            logo={require("../images/ifr.svg")}
+            image={require("../images/ifr.jpg")}
+          />
           <Tool
             class="Tool STN"
             link="/stn"
