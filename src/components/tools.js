481eb4c038e94636dbe60b6a5b5c5e06c9f8f374
Jonathan Edward Lee <jonathan.lee02@sap.com>
2019-08-07 14:32:17 +0200

    update mobile css

--- src/components/tools.js
+++ src/components/tools.js
@@ -91,6 +91,62 @@ export default class Tools extends React.Component {
             logo={require("../images/ifr.svg")}
             image={require("../images/ifr.jpg")}
           />
+          <Tool
+            class="Tool STN"
+            link="/stn"
+            title="SAP Transformation Navigator"
+            logo={require("../images/stn.svg")}
+            image={require("../images/stn.jpg")}
+          />
+          <Tool
+            class="Tool Sirius"
+            link="/sirius"
+            title="Sirius"
+            logo={require("../images/sirius.svg")}
+            image={require("../images/sirius.jpg")}
+          />
+          <Tool
+            class="Tool PPMS"
+            link="/ppms"
+            title="PPMS Light"
+            logo={require("../images/ppms.svg")}
+            image={require("../images/ppms.jpg")}
+          />
+          <Tool
+            class="Tool Polaris"
+            link="/polaris"
+            title="Polaris"
+            logo={require("../images/polaris.svg")}
+            image={require("../images/polaris.jpg")}
+          />
+          <Tool
+            class="Tool Lucid"
+            link="/lucid"
+            title="Lucid"
+            logo={require("../images/lucid.svg")}
+            image={require("../images/lucid.jpg")}
+          />
+          <Tool
+            class="Tool Solaris"
+            link="/solaris"
+            title="Solaris"
+            logo={require("../images/solaris.svg")}
+            image={require("../images/solaris.jpg")}
+          />
+          <Tool
+            class="Tool Toby"
+            link="/tobi"
+            title="Tobi"
+            logo={require("../images/toby.svg")}
+            image={require("../images/toby.jpg")}
+          />
+          <Tool
+            class="Tool IFR"
+            link="/ifr"
+            title="Innovation & Feature Repository"
+            logo={require("../images/ifr.svg")}
+            image={require("../images/ifr.jpg")}
+          />
         </ItemsCarousel>
       </div>
     )
