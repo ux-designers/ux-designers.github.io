3b1e599b99f119e85f43aa8f8b415eb3c9ec4f22
Sarina Claudia Walter <sarina.claudia.walter@sap.com>
2020-12-10 14:24:17 +0100

    problem with job posting link (therefore excluded)

--- src/components/jobs.js
+++ src/components/jobs.js
@@ -22,9 +22,9 @@ class Jobs extends React.Component {
         </div>
 
 
-        {/*<div className="JobSection">
+        <div className="JobSection">
           <h6>code.</h6>
-          <Job
+          {/*<Job
             joblink="https://performancemanager5.successfactors.eu/acme?bplte_company=SAP&fbacme_n=recruiting&recruiting%5fns=joblisting%20detail&itrModule=rcm&recruiting_mode=266576&selected_lang=en_US&jobAlertController_jobAlertId=&jobAlertController_jobAlertName=&_s.crb=X1iWGt%2fHquPC0Xyq8LL1FB31cNi%2fA4fzvApjj%2fh4lrc%3d"
             jobtitle="Progressive Delivery (Senior) Developer (f/m/d)​​​​​"
           />
@@ -47,9 +47,9 @@ class Jobs extends React.Component {
           <Job
             joblink="https://performancemanager5.successfactors.eu/acme?bplte_company=SAP&fbacme_n=recruiting&recruiting%5fns=joblisting%20detail&itrModule=rcm&recruiting_mode=266785&selected_lang=en_US&jobAlertController_jobAlertId=&jobAlertController_jobAlertName=&_s.crb=X1iWGt%2fHquPC0Xyq8LL1FB31cNi%2fA4fzvApjj%2fh4lrc%3d"
             jobtitle="Full Stack Developer (f/m/d)"
-          /> 
+          /> */}
           <p>- Currently no vacancy available. -</p>
-    </div>*/}
+    </div>
       </div>
     )
   }
